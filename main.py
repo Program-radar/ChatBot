d5f03076a7c4c06c3064e3e5255ed272b35bedcf
mehran-ahmadzadeh <ahmadzadeh_dev@protonmail.com>
2020-12-30 11:32:53 +0300

    add messages and responses with in a dictionary

---
+++ main.py
@@ -0,0 +1,8 @@
+# chat bot main file
+responses = {
+    "hello": "Hello, what's up ?",
+    "how is it going ?": "Good, How about you ?",
+    "how are you": "I am feel fantastic",
+    "what is your name": "I am Chat Bot builds with python you can call anything.",
+    "what do you do ?":  "I chat with you. You can tell me anything. I won't tell anyone about it, Trust Me!"
+}
