4c84e3fbf350ff190943dfde8312bf853a906d47
mehran-ahmadzadeh <ahmadzadeh_dev@protonmail.com>
2020-12-30 11:51:30 +0300

    add pyttsx3 for text to speech conversion

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # chat bot main file
+import pyttsx3 as voice
+
+
 responses = {
     "hello": "Hello, what's up ?",
     "how is it going ?": "Good, How about you ?",
@@ -6,3 +9,10 @@ responses = {
     "what is your name": "I am Chat Bot builds with python you can call anything.",
     "what do you do ?":  "I chat with you. You can tell me anything. I won't tell anyone about it, Trust Me!"
 }
+
+# initializing voice
+voice_engine = voice.init()
+
+voice_engine.say("Hello, I am virtual assistant. How can I help you ?")
+
+voice_engine.runAndWait()
