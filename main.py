b7ca2553111b82bfe629e665770d0d60a935587a
mehran-ahmadzadeh <ahmadzadeh_dev@protonmail.com>
2020-12-31 08:32:47 +0300

    merge both voice and text chatting into one process

--- main.py
+++ main.py
@@ -10,49 +10,41 @@ responses = {
     "what do you do ?":  "I chat with you. You can tell me anything. I won't tell anyone about it, Trust Me!"
 }
 
-# asking user to choose between text and voice
-text_or_voice = input("Welcome to the Chat Bot! How do you like to chat ?[text/voice] ")
-quit_chat = True
+# initializing voice
+voice_engine = voice.init()
 
-if text_or_voice == "text":
-    print("Type 'q' at any time to quit")
-    while quit_chat:
-        message = input("\nType your message: ")
-        if message == "q":
-            quit_chat = False
-        else:
-            try:
-                print(responses[message])
-            except KeyError:
-                print("I can't understand, Suggestions:\n"
-                      "   Make sure all words are spelled correctly.\n"
-                      "   Try different keywords.\n"
-                      "   Try more general keywords.\n"
-                      "   Try fewer keywords")
 
-elif text_or_voice == "voice":
-    # initializing voice
-    voice_engine = voice.init()
-
-    print("Type 'q' at any time to quit")
-    voice_engine.say("Start Chatting with me.\n"
-                     "Remember you can quit chat by typing 'q' at any time.")
+# print first element and say second element
+# if program need something else to say pass argument to second parameter
+# if not, don't pass it and program will say exactly what printed
+def print_and_say(message_to_print, message_to_say=None):
+    print(message_to_print)
+    if message_to_say is None:
+        voice_engine.say(message_to_print)
+    else:
+        voice_engine.say(message_to_say)
     voice_engine.runAndWait()
 
-    while quit_chat:
-        message = input("\nType your message: ")
-        if message == "q":
-            voice_engine.say("It was good talk. See you Later!")
+
+quit_chat = True
+
+# start chat process
+print_and_say("Type 'q' at anytime to quit. ",
+              "Start chatting.\n Remember you can quit at any time by Typing 'q' ")
+while quit_chat:
+    message = input("\nType your message: ")
+
+    if message == "q":
+        # quit chat
+        print_and_say("It was good talk, See you later!")
+    else:
+        try:
+            print_and_say(responses[message])
+        except KeyError:
+            # if response not found
+            print_and_say("I can't understand, Suggestions:\n"
+                          "   Make sure all words are spelled correctly.\n"
+                          "   Try different keywords.\n"
+                          "   Try more general keywords.\n"
+                          "   Try fewer keywords.")
             voice_engine.runAndWait()
-            quit_chat = False
-        else:
-            try:
-                voice_engine.say(responses[message])
-                voice_engine.runAndWait()
-            except KeyError:
-                voice_engine.say("I can't understand, Suggestions:\n"
-                                 "   Make sure all words are spelled correctly.\n"
-                                 "   Try different keywords.\n"
-                                 "   Try more general keywords.\n"
-                                 "   Try fewer keywords")
-                voice_engine.runAndWait()
